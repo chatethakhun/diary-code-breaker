8929946c062f32415c4e962b74146e200b57505b
chatethakhun <chatethakhun@hubbathailand.com>
2021-02-13 23:38:14 +0700

    add av page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,10 @@ class PagesController < ApplicationController
   def cooking_run
     @coupons = Coupon.all
   end
+
+  def avgle
+    page = 0
+    limit = 1
+    @list_av = HTTParty.get("https://api.avgle.com/v1/videos/#{page}?limit=#{limit}")['response']['videos']
+  end
 end
