d049fea59033585f67492c73baea22e7f0b128d5
chatethakhun <chatethakhun@hubbathailand.com>
2021-02-14 00:23:15 +0700

    add pagination

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,11 @@ class PagesController < ApplicationController
   end
 
   def avgle
-    page = 0
-    limit = 10
-    @list_av = HTTParty.get("https://api.avgle.com/v1/videos/#{page}?limit=#{limit}")['response']['videos']
+    page = params[:page].present? ? params[:page].to_i : 0
+    limit = 40
+    response = HTTParty.get("https://api.avgle.com/v1/videos/#{page}?limit=#{limit}")
+    @tatal_pag_tab = response['response']['total_videos'] / limit
+    @list_av = response['response']['videos']
   end
 
   def av_show
