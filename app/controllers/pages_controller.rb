c5523951d74b9c2d318c2f13a380b656251b7d8b
chatethakhun <chatethakhun@hubbathailand.com>
2021-02-14 00:01:55 +0700

    redesign av page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,12 @@ class PagesController < ApplicationController
 
   def avgle
     page = 0
-    limit = 1
+    limit = 10
     @list_av = HTTParty.get("https://api.avgle.com/v1/videos/#{page}?limit=#{limit}")['response']['videos']
+  end
+
+  def av_show
+    vid = params[:vid]
+    @av = HTTParty.get("https://api.avgle.com/v1/videos/#{vid}")['response']['videos']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   end
 
   get :avgle, to: 'pages#avgle'
+  get '/avgle/:vid', to: 'pages#avgle_show', as: :av_detail
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
