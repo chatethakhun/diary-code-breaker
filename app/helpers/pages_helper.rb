e9f356853043b8e916d26169bce1fd1803aec7ee
chatethakhun <chatethakhun@hubbathailand.com>
2021-02-14 14:43:44 +0700

    redesign card

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,4 +4,11 @@ module PagesHelper
   def get_badge_class
     BADGES.sample
   end
+
+  def formatted_duration(total_seconds)
+    hours = total_seconds / (60 * 60)
+    minutes = (total_seconds / 60) % 60
+
+    "#{hours > 0 ? "#{hours} h" : ""} #{ minutes > 0 ? "#{minutes} m" : "" }"
+  end
 end
