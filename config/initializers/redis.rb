43f14bbf91554647f23e002b4b6fcf4dd28e05c9
chatethakhun <chatethakhun.jack@gmail.com>
2022-03-22 17:36:01 +0700

    add redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,1 @@
-Redis.current = Redis.new(:host => ( ENV["REDIS_URL"] ),
-                          :port => ( ENV["REDIS_PORT"] ).to_i,
-                          :password => ( ENV["REDIS_PASSWORD"] ))
+$redis = Redis.new(:password=>ENV['REDIS_PASSWORD'])
